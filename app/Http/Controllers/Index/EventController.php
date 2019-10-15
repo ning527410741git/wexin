6dc0533ae7af3da030c9503e22002ff891e1db50
ning527410741git <527410741@qq.com>
2019-10-15 18:43:20 +0800

    a

--- app/Http/Controllers/Index/EventController.php
+++ app/Http/Controllers/Index/EventController.php
@@ -13,5 +13,6 @@ class EventController extends Controller
         file_put_contents(storage_path('logs/wexin/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
         $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
         $xml_arr = (array)$xml_obj;
+    //         echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
     }
 }
