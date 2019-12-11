2427c927a2fb4efc18dff87a25043def5f0ef643
ning527410741git <527410741@qq.com>
2019-12-11 19:02:29 +0800

    first commit

--- app/Http/Controllers/Admin/GoodsController.php
+++ app/Http/Controllers/Admin/GoodsController.php
@@ -8,8 +8,7 @@ use App\Http\Controllers\Controller;
 class GoodsController extends Controller
 {
    	public function addGoo(Request $request){
-   		$data=file_get_contents('http://127.0.0.1:8848/HB/index.html');
-   		 $resurl=json_decode($data,1);
-   		 dd($resurl);
+   		$data=$request->all();
+   		 dd($data);
    	}
 }
